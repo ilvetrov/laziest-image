3b4f4f10255be2924ac81304e464ac3313d1df89
ilvetrov <ilya.ilvetrov@gmail.com>
2023-03-01 01:16:43 +0400

    Basic E2E

---
+++ e2e/outerCore/outer.ts
@@ -0,0 +1,7 @@
+import { VirtualImage } from "../../src/core/LazyImage/VirtualImage"
+import { ExtraWindow } from "./core/ExtraWindow"
+
+export const extraWindow = ExtraWindow({
+  VirtualImage,
+  components: []
+})
